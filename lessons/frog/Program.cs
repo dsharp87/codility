674a78317de6f3ec042a7ec88701d3fa54dc809c
dswayne599 <dswayne599@gmail.com>
2021-04-16 14:42:08 -0700

    adding permmissingelement

---
+++ lessons/frog/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace frog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int solution(int X, int Y, int D) 
+        {
+            int jumps = 0;
+            while(X < Y)
+            {
+                X+= D;
+                jumps++;
+            }
+            return jumps;
+        }
+    }
+}
---
+++ lessons/permmissingelement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace permmissingelement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(solution(new int[]{4,1,2}));
+            solution(new int[] {1,2});
+        }
+
+        public static int solution(int[] A) {
+            
+            //array can be empty
+            if(A.Length == 0)
+            {
+                return 1;
+            }
+            //array is not sorted
+            //must be efficient
+            //could make an array the same lenght  + 1
+            //then add all values
+            //then find index of the 0
+
+            int[] copy = new int[A.Length + 1];
+            for(int i = 0; i < A.Length; i++)
+            {
+                copy[A[i] -1 ] = A[i];
+            }
+
+            return Array.IndexOf(copy, 0) + 1;
+        }
+
+
+    }
+}
