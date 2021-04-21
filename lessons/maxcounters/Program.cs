e17562c71981866a165e3bde6d3212f28056d799
dswayne599 <dswayne599@gmail.com>
2021-04-21 10:54:02 -0700

    adding maxcounters

---
+++ lessons/maxcounters/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace maxcounters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 5;
+            int[] nums = new int[] {3,4,4,6,1,4,4};
+            solution(count, nums);
+        }
+
+        public static int[] solution(int N, int[] A) 
+        {
+            int max = 0;
+            int maxAtmaxHit = 0;
+            int[] result = new int[N];
+            bool maxflag = false;
+            for(int i = 0; i < A.Length; i++)
+            {
+                int idx = A[i] - 1;
+                if(idx != N)
+                {
+                    if(maxflag && result[idx] <= maxAtmaxHit)
+                    {
+                        result[idx] = maxAtmaxHit + 1;
+                    }
+                    else
+                    {
+                        result[idx]++;
+                    }
+                    max = result[idx] > max ? result[idx] : max; 
+                }
+                else
+                {
+                    maxflag  = true;
+                    maxAtmaxHit = max;
+                }
+            }
+            for(int i = 0; i < result.Length; i++)
+            {
+                if(result[i] < maxAtmaxHit)
+                {
+                    result[i] = maxAtmaxHit;
+                }
+            }
+            return result;
+        }
+
+        
+    }
+}
