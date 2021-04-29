da5b67c04369e0b9e004723b29dfbca6e106ce7c
dswayne599 <dswayne599@gmail.com>
2021-04-29 15:39:08 -0700

    adding passingCars

---
+++ lessons/passingCars/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace passingCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(solution(new int[] {0,1,0,1,1}));
+        }
+
+        public static int solution(int[] A)
+        {
+            //need a count of how many of eastbund 
+            //when a west bound is found, add how many east we've seen already
+
+            int eastCount = 0;
+            long result = 0;
+
+            for(int i = 0; i<A.Length; i++)
+            {
+                if(A[i] == 0)
+                {
+                    eastCount++;
+                }
+                else
+                {
+                    result += eastCount;
+                }
+            }
+
+            return result <= 1000000000 ? (int)result : -1;
+        }
+    }
+}
